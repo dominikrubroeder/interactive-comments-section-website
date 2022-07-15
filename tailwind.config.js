be507e59e957b86121fa8a32d7abdc2feee93687
Dominik <dominik.rubroeder@icloud.com>
2022-07-15 15:42:28 +0200

    Add custom app colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'app-primary-blue-moderate': 'hsl(238, 40%, 52%)',
+        'app-primary-red-soft': 'hsl(358, 79%, 66%)',
+        'app-primary-blue-grayish-light': 'hsl(239, 57%, 85%)',
+        'app-primary-red-pale': 'hsl(357, 100%, 86%)',
+        'app-neutral-blue-dark': 'hsl(212, 24%, 26%)',
+        'app-neutral-blue-grayish': 'hsl(211, 10%, 45%)',
+        'app-neutral-gray-light': 'hsl(223, 19%, 93%)',
+        'app-neutral-gray-light-very': 'hsl(228, 33%, 97%)',
+      },
+    },
   },
   plugins: [],
 };
